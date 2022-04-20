8b9848a93edbc8a8db4efc957b731e53c79ad455
Kwizera Steven <kwiste06@gmail.com>
2022-04-20 09:25:22 +0200

    seting up trailwind-css in our app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from "react"
+
+
 function App() {
     return (<div>
-        <h2>Welcome To Phantom</h2>
-        <h3>Date : {new Date().toDateString()}</h3>
+        <h2 className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-4 mt-4">Welcome To Phantom</h2>
+        <h3 className="text-3xl font-bold underline">Date : {new Date().toDateString()}</h3>
+       
     </div>)
 }
 export default App;
+

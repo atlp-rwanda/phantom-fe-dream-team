97d8817038d6b566f05fbd098d0f152ee027ef62
gyssa-prince <mhzprnc@gmail.com>
2022-05-03 19:48:40 +0200

    updating popups

--- src/components/error.js
+++ src/components/error.js
@@ -3,8 +3,7 @@ import Error from './img/errorIcon.png';
 function ErrorPopup (props){
     return (props.trigger)?(
         <div class="ml-4 mt-px fixed top-0 left-0 w-80 h-30 bg-blue-500 justify-center items-center flex-initial  ml-[820px] mt-40">
-            <div class="relative p-12 w-full text-white content-center ">
-            <button onClick={()=>setSucceed(false)} className="absolute top-0 right-2">X</button>    
+            <div class="relative p-12 w-full text-white content-center "> 
             <Icon icon="bxs:error" width="32" height="32" className="absolute top-2 right-40 h-8 w-8" />           
             <img className=" absolute top-2 right-40 h-8 w-8  " src={Error} alt="succed icon"  />
                 {props.children}
--- src/components/succesfull.js
+++ src/components/succesfull.js
@@ -4,7 +4,6 @@ function SuccefullPopup (props){
     return (props.trigger)?(
         <div class="ml-0 mt-1 fixed top-0 left-0 w-80 h-30 bg-blue-500 justify-center items-center flex-initial  ml-[820px] mt-40">
             <div class="relative p-12 w-full text-white content-center ">
-            <button onClick={()=>close()} className="absolute top-0 right-2">X</button>               
              <img className=" absolute top-2 right-40 h-8 w-8  " src={Vector} alt="succed icon"  />
                 
                 {props.children}

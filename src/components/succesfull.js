0389fa2ad3a30fc17956884d1820900d4e8adb4c
gyssa-prince <mhzprnc@gmail.com>
2022-05-06 15:30:49 +0200

    Solve errors

--- src/components/succesfull.js
+++ src/components/succesfull.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Vector from './img/vector.png';
+import { Icon } from '@iconify/react';
 function SuccefullPopup (props){
     return (props.trigger)?(
         <div class="ml-0 mt-1 fixed top-0 left-0 w-80 h-30 bg-blue-500 justify-center items-center flex-initial  ml-[820px] mt-40">
             <div class="relative p-12 w-full text-white content-center ">
-             <img className=" absolute top-2 right-40 h-8 w-8  " src={Vector} alt="succed icon"  />
+            <p className=" absolute top-2 right-40 h-8 w-8 " ><Icon icon="flat-color-icons:ok"  width="36" height="36"  /></p>
                 
                 {props.children}
             </div>

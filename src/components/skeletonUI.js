004c6d554f3e15e02a32f5675157c36196869569
gyssa-prince <mhzprnc@gmail.com>
2022-05-09 11:58:30 +0200

    Update

--- src/components/skeletonUI.js
+++ src/components/skeletonUI.js
@@ -3,7 +3,7 @@ import loader from './img/loading.gif'
 export default function skeletonUI() {
   return (
     <div className="flex flex-col items-center">
-      <img src={loader} alt='' className='pt-32 w-72 h-70'/>
+      <img src={loader} alt='loading'/>
     </div>
   )
 }
--- src/components/succesfull.js
+++ src/components/succesfull.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { Icon } from '@iconify/react';
 function SuccefullPopup (props){
     return (props.trigger)?(
-        <div class="ml-0 mt-1 fixed top-0 left-0 w-80 h-30 bg-blue-500 justify-center items-center flex-initial  ml-[820px] mt-40">
+        <div class="ml-1 mt-1 fixed top-0 left-0 bg-blue-700 mt-[70px]">
             <div class="relative p-12 w-full text-white content-center ">
-            <p className=" absolute top-2 right-40 h-8 w-8 " ><Icon icon="flat-color-icons:ok"  width="36" height="36"  /></p>
+            <p className=" absolute top-2 right-[140px] h-8 w-8 " ><Icon icon="flat-color-icons:ok"  width="36" height="36"  /></p>
                 
                 {props.children}
             </div>

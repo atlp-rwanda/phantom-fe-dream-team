b9213e6d007d7c185fb58c0cc56db05d281ea857
stevenkwizera06 <kwiste06@gmail.com>
2022-05-11 15:13:35 +0200

    chore(add):add font Poppins
    adding font poppins in our app
    
    [ Finishes #08 ]

--- src/components/Landingpage/Landingpage.js
+++ src/components/Landingpage/Landingpage.js
@@ -10,7 +10,7 @@ function Landingpage() {
 
             <div className='flex  flex-row  mt-20 py-10 xs:flex-col-reverse md:mt-10'>
                 <div className='text-center mt-20 xs:m-4 px-20 lg:mt-10 '>
-                    <h1 className='text-5xl text-blue-700 font-serif font-bold xl:text-5xl lg:text-5xl md:text-4xl sm:text-2xl xs:text-base xs:mt-2 xs:py-2'>
+                    <h1 className='text-5xl text-blue-700  font-bold xl:text-5xl lg:text-5xl md:text-4xl sm:text-2xl xs:text-base xs:mt-2 xs:py-2'>
                         Track Bus Location
                     </h1>
                     <p className='text-serif py-10 text-blue-500 text-xl xl:text-base lg:text-base md:text-base sm:text-xs xs:text-xs xs:mt-2 xs:py-2' >Choose Your Route Track your bus live .</p>

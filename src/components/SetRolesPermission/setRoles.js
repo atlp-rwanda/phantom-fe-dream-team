04f87001b11c727fd214860f5892157464e3450f
gyssa-prince <mhzprnc@gmail.com>
2022-05-17 20:46:30 +0200

    Add Edit role name

--- src/components/SetRolesPermission/setRoles.js
+++ src/components/SetRolesPermission/setRoles.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Icon } from '@iconify/react';
-import {  useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import user from '../img/random.jpg';
 import SuccefullPopup from '../succesfull';
 import { Route, Link, Routes, useLocation } from "react-router-dom";
@@ -8,25 +8,23 @@ import ErrorPopup from "../error";
 import { useLoader } from '../useLoader';
 import SkeletonUI from '../skeletonUI';
 import AddRole from "./AddRole";
-
-import { setPermission } from "../../redux/actions/index";
-import data from './data.json';
+import {setPermission} from "../../redux/actions/index";
 import { info } from "autoprefixer";
 import TopNavbar from "../Dashboard/TopNavbar";
 
-function setRoles() {
+function setRoles (){
 
 
   const dispatch = useDispatch();
   const [Infos, setData] = useState(null);
   const [Permissions, setPermissions] = [{
-    AddEditDelOp: useState(false),
-    viewDelOp: useState(false),
-    AssgnRemDriv: useState(false),
-    addRemRoute: useState(false),
-    UpdateBusInfo: useState(false),
-    UpdateProf: useState(false),
-  }];
+    AddEditDelOp:useState(false),
+    viewDelOp:useState(false),
+    AssgnRemDriv:useState(false),
+    addRemRoute:useState(false),
+    UpdateBusInfo:useState(false),
+    UpdateProf:useState(false),
+   }];
 
   useEffect(() => {
     fetch('http://localhost:8000/Permissions')
@@ -37,99 +35,119 @@ function setRoles() {
         setData(data);
       })
   }, [])
+//
 
-  //
+// Permissions.map((Perm)=>(
+//     loadForm()
+//   ))
 
-  // Permissions.map((Perm)=>(
-  //     loadForm()
-  //   ))
-
-  function loadForm(info) {
-    const Perm = info.Permissions;
-    console.log(Perm.AddEditDelOp);
-    if (Perm.AddEditDelOp == true) {
-      document.getElementById('One' + (info.id)).checked = true;
-    }
-    if (Perm.viewDelOp == true) {
-      document.getElementById('Two' + (info.id)).checked = true;
-    }
-    if (Perm.AssgnRemDriv == true) {
-      document.getElementById('Three' + (info.id)).checked = true;
-    }
-    if (Perm.addRemRoute == true) {
-      document.getElementById('Four' + (info.id)).checked = true;
-    }
-    if (Perm.UpdateBusInfo == true) {
-      document.getElementById('Five' + (info.id)).checked = true;
-    }
-    if (Perm.UpdateProf == true) {
-      document.getElementById('Six' + (info.id)).checked = true;
-    }
-
-    // let i, j;
-    // for (i = 0; i < 3; i++) {
-    //   for (j = 0; j < 5; j++) {
-    //     if ( == 'true') {
-    //       if (j == 0) {
-    //         document.getElementById('read' + (i + 1)).checked = true;
-    //       }
-    //       else if (j == 1) {
-    //         document.getElementById('add' + (i + 1)).checked = true;
-    //       }
-    //       else if (j == 2) {
-    //         document.getElementById('edit' + (i + 1)).checked = true;
-    //       }
-    //       else if (j == 3) {
-    //         document.getElementById('delete' + (i + 1)).checked = true;
-    //       }
-
-    //     }
-    //   }
-    // }
-  }
-
-
-  function submitForm(id) {
-    const OneInput = document.getElementById('One' + id);
-    Permissions.AddEditDelOp = OneInput.checked ? true : false;
-    const TwoInput = document.getElementById('Two' + id)
-    Permissions.viewDelOp = TwoInput.checked ? true : false;
-
-    const ThreeInput = document.getElementById('Three' + id)
-    Permissions.AssgnRemDriv = ThreeInput.checked ? true : false;
-
-    const FourInput = document.getElementById('Four' + id)
-    Permissions.addRemRoute = FourInput.checked ? true : false;
-    const FiveInput = document.getElementById('Five' + id)
-    Permissions.UpdateBusInfo = FiveInput.checked ? true : false;
-    const SixInput = document.getElementById('Six' + id)
-    Permissions.UpdateProf = SixInput.checked ? true : false;
-
-    const role = Permissions;
-
-
-    console.log('Role:', role)
-    fetch('http://localhost:8000/Permissions/' + id, {
-      method: 'PATCH',
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(
-        {
-          "Permissions": { "AddEditDelOp": Permissions.AddEditDelOp, "viewDelOp": Permissions.viewDelOp, "AssgnRemDriv": Permissions.AssgnRemDriv, "addRemRoute": Permissions.addRemRoute, "UpdateBusInfo": Permissions.UpdateBusInfo, "UpdateProf": Permissions.UpdateProf }         // we are changing the "likes" value to 5
+function loadForm(info) {
+  const Perm= info.Permissions;
+  console.log(Perm.AddEditDelOp);
+  if (Perm.AddEditDelOp == true) {
+         document.getElementById('One' + (info.id)).checked = true;
+         }
+   if (Perm.viewDelOp == true) {
+          document.getElementById('Two' + (info.id)).checked = true;
         }
-      )
-    }).then(() => {
-      console.log('new role added');
-    })
+   if (Perm.AssgnRemDriv == true) {
+          document.getElementById('Three' + (info.id)).checked = true;
+        }
+  if (Perm.addRemRoute == true) {
+          document.getElementById('Four' + (info.id)).checked = true;
+     }     
+  if (Perm.UpdateBusInfo == true) {
+      document.getElementById('Five' + (info.id)).checked = true;
+    }   
+  if (Perm.UpdateProf == true) {
+      document.getElementById('Six' + (info.id)).checked = true;
+    }     
+    
+  // let i, j;
+  // for (i = 0; i < 3; i++) {
+  //   for (j = 0; j < 5; j++) {
+  //     if ( == 'true') {
+  //       if (j == 0) {
+  //         document.getElementById('read' + (i + 1)).checked = true;
+  //       }
+  //       else if (j == 1) {
+  //         document.getElementById('add' + (i + 1)).checked = true;
+  //       }
+  //       else if (j == 2) {
+  //         document.getElementById('edit' + (i + 1)).checked = true;
+  //       }
+  //       else if (j == 3) {
+  //         document.getElementById('delete' + (i + 1)).checked = true;
+  //       }
 
-  }
-  function Delete(id) {
-    fetch('http://localhost:8000/Permissions/' + id, {
-      method: 'DELETE'
-    }).then(() => {
-      window.reload()
-    })
-  }
-  const { loading } = useLoader();
+  //     }
+  //   }
+  // }
+}
+
+
+function submitForm(id) {
+  const OneInput = document.getElementById('One'+id);
+  Permissions.AddEditDelOp = OneInput.checked ? true : false;
+  const TwoInput = document.getElementById('Two'+id)
+  Permissions.viewDelOp = TwoInput.checked ? true : false;
+
+  const ThreeInput = document.getElementById('Three'+id)
+  Permissions.AssgnRemDriv = ThreeInput.checked ? true : false;
+
+  const FourInput = document.getElementById('Four'+id)
+  Permissions.addRemRoute = FourInput.checked ? true : false;
+  const FiveInput = document.getElementById('Five'+id)
+  Permissions.UpdateBusInfo = FiveInput.checked ? true : false;
+  const SixInput = document.getElementById('Six'+id)
+  Permissions.UpdateProf = SixInput.checked ? true : false;
+  const Role=document.getElementById("role"+id).value;
+  
+  
+
+
+if(Role!=''){
+  fetch('http://localhost:8000/Permissions/'+id, {
+    method: 'PATCH',
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(
+      {
+        "Permissions": {"AddEditDelOp":Permissions.AddEditDelOp,"viewDelOp":Permissions.viewDelOp, "AssgnRemDriv":Permissions.AssgnRemDriv,"addRemRoute":Permissions.addRemRoute,"UpdateBusInfo":Permissions.UpdateBusInfo,"UpdateProf":Permissions.UpdateProf},
+        "Role":Role
+      }
+    )
+  }).then(() => {
+    console.log('role and permissions updated');
+  })
+}
+else{
+  fetch('http://localhost:8000/Permissions/'+id, {
+    method: 'PATCH',
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(
+      {
+        "Permissions": {"AddEditDelOp":Permissions.AddEditDelOp,"viewDelOp":Permissions.viewDelOp, "AssgnRemDriv":Permissions.AssgnRemDriv,"addRemRoute":Permissions.addRemRoute,"UpdateBusInfo":Permissions.UpdateBusInfo,"UpdateProf":Permissions.UpdateProf}
+      }
+    )
+  }).then(() => {
+    console.log('permissions updated');
+  })
+}
+}
+
+
+function Delete(id){
+  fetch('http://localhost:8000/Permissions/' + id, {
+    method: 'DELETE'
+  }).then(() => {
+    window.reload()
+  }) 
+}
+function Editable(id){
+  document.getElementById('role'+id).readOnly = false;
+  
+}
+const { loading } = useLoader();
   return (
     <>
     <TopNavbar goto={e=>window.location.assign('/dashboard/Roles/add')}/>
@@ -152,7 +170,7 @@ function setRoles() {
                 loadForm(info)
               }, "1000"),
               <tr className="mb-12  h-8 text-xl hover:border-solid border-solid border-2 border-black hover:border-2 border-blue-600 drop-shadow-md hover:drop-shadow-2xl">
-                <td className="text-lg font-bold "><input type="text" id='role1' placeholder={info.Role} />
+                <td className="text-lg font-bold " onClick={()=>Editable(info.id)}><input type="text" id={'role'+info.id} placeholder={info.Role} readOnly />
                   <h3>
                     Description: <br />
                     {info.Description}

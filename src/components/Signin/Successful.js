d6632e7a9197cb4cbc9a3ced2a070cf498d87346
Juleshb <93904693+Juleshb@users.noreply.github.com>
2022-05-18 17:35:51 +0200

    feat(SignIn): Login Form (#14)
    * feat(SignUp): Login Form
    
    Appropriate error messages are displayed on invalid inputs (Form validation).
    Appropriate error messages are displayed on unsuccessful attempts
    The page is responsive
    
    WIP #29
    
    * Rename Login file
    
    * Validation
    
    * updated login states
    
    * feat(SignUp): Login Form
    
    Appropriate error messages are displayed on invalid inputs (Form validation).
    Appropriate error messages are displayed on unsuccessful attempts
    The page is responsive
    
    WIP #29
    
    rebased the login branch with develop#
    
    * updated login component
    
    * Validation
    
    * updated login states
    
    * fixed conflicts and rebased the branch to the develop
    
    * updated signin css
    
    * Add skelton
    
    * added a well designed pop up
    
    * updated coverege
    
    * packages
    
    * updated redux fonctinalities
    
    * adding Forgten password bouton
    
    * Changing color
    
    * updated signin redux
    
    * updated signin redux
    
    * Changing color
    
    * adding porp up
    
    * updated login component with redux
    
    * making page responsiveness
    
    * updating error messages
    
    Co-authored-by: Rukundo725 <gerukundo14@gmail.com>

---
+++ src/components/Signin/Successful.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Vector from '../../assets/vector.png';
+function SuccefullPopup (props){
+    return (props.trigger)?(
+        <div class="ml-1 mt-1 fixed top-0 left-0 bg-blue-700 mt-[70px] z-10">
+            <div class="relative p-12 w-full text-white">
+            <div className='flex justify-center'>
+            <img className="h-8 w-8  " src={Vector} alt="succed icon"  />
+            </div>
+            <p className='mt-5'>{props.children}</p>
+            </div>
+        </div>
+    ) :"";
+}
+export default SuccefullPopup;

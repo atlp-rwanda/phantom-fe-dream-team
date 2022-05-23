e2ab04eb660cbd3b8a6964781c2d9cbf86de5e89
Ben Africa <byiringiro5ben10@gmail.com>
2022-05-23 12:11:27 +0200

    resolved conflicts

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,27 +1,21 @@
 import updateProfile from "./updateProfile";
 import changePassword from "./changepassword";
 import  {resetReducer}  from "./resetReducers"; 
-<<<<<<< HEAD
 import routesReducer from "./routesSlice";
-=======
 import LoginReducer from "./LoginReducer";
 import { authReducer } from './authReducer';
 
->>>>>>> develop
 //import other reducers
 import { combineReducers } from "redux";
 
 const allReducers = combineReducers({
   
     resetReducer,
-<<<<<<< HEAD
-    routesReducer
+    routesReducer,
 
-=======
     LoginReducer,
     updateProfile,changePassword,
     resetReducer,authReducer
->>>>>>> develop
     //other reducers
 });
 

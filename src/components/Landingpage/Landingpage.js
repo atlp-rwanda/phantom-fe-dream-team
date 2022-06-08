e115790312e20d9e009f1c77a715787af7025108
Juleshb <habaruremajules@gmail.com>
2022-06-08 14:37:28 +0200

    increse sise of bus slide

--- src/components/Landingpage/Landingpage.js
+++ src/components/Landingpage/Landingpage.js
@@ -5,8 +5,8 @@ function Landingpage() {
     return (
         <div>
  
-            <div className='flex  flex-row  mt-20 py-10 xs:flex-col-reverse md:mt-10 '>
-                <div className='text-center mt-20 xs:m-4 px-20 lg:mt-10 '>
+            <div className=" flex space-x-reverse flex-row mt-20 py-10 xs:flex-col-reverse md:mt-10 justify-center ">
+                <div className='text-center mt-20 ml-20 xs:m-4 px-20 lg:mt-10 '>
                     <h1 className='text-5xl text-blue-700  font-bold xl:text-5xl lg:text-5xl md:text-4xl sm:text-2xl xs:text-base xs:mt-2 xs:py-2'>
                         Track Bus Location
                     </h1>
@@ -16,7 +16,7 @@ function Landingpage() {
 
 
                 </div>
-                <div className='mt-20 h-20 w-30 mr-10 xs:m-4 sm:hiden'>
+                <div className='mt-20 h-20 w-30 mr-30 xs:m-4 sm:hiden '>
                 <Slider />
 
                 </div>

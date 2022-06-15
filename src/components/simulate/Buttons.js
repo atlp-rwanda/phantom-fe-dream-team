4a6b5ad0ec4d3b59ab6a9a34535ac6633b2a5d2f
shemalucien <shemalucien5@gmail.com>
2022-06-15 10:28:34 +0200

    add and remove passengers

--- src/components/simulate/Buttons.js
+++ src/components/simulate/Buttons.js
@@ -13,34 +13,5 @@ const Button = ({ name, styles, onClick }) => {
     </div>
   );
 };
-export const ButtonA = ({ name, onClick, styles, type }) => {
-  return (
-    <div>
-      <button
-        type={type}
-        onClick={onClick}
-        className={`${styles} bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-xl m-2 `}
-      >
-        {name}
-      </button>
-    </div>
-  );
-};
 
 export default Button;
-
-export const ButtonLoading = ({ name }) => {
-  return (
-    <div className="flex w-fit py-1 px-3 bg-primary font-raleway font-bold rounded-md">
-      <div className="loader mr-1"></div><button className='ml-1 font-raleway text-white font-bold'> {name}</button>
-    </div>
-  );
-};
-
-export const PrimaryButton = ({ name, type="button", ...options }) => {
-  return (
-    <button {...options} type={type} className="bg-primary rounded-md text-white font-bold font-raleway py-2 px-3 hover:bg-hover transition-all block hover:transition-all">
-      {name}
-    </button>
-  );
-};
--- src/components/simulate/TrackingPage.js
+++ src/components/simulate/TrackingPage.js
@@ -36,8 +36,10 @@ const TrackingPage = () => {
   const selectOne = useRef();
   const selectDes = useRef();
   const [position, setposition] = useState(null);
-
+  const [passengers, setPassengers] = useState(0);
   const handleRoute = (e) => {
+
+
     e.preventDefault();
     options.filter((option) => {
       const startingPoint = option.name === e.target.origin.value;
@@ -142,6 +144,20 @@ const TrackingPage = () => {
     });
   };
 
+  const addPassenger = () => {
+    setPassengers((prev) => prev + 1);
+    alert("Added one Passenger");
+  };
+
+  const removePassenger = () => {
+    if (passengers !== 0) {
+      setPassengers((prev) => prev - 1);
+      alert("Removed one Passenger");
+    } else {
+      alert("No Passengers To Remove");
+    }
+  };
+
   useEffect(() => {
     if ('geolocation' in navigator) {
       navigator.geolocation.getCurrentPosition((pos) => {
@@ -155,213 +171,137 @@ const TrackingPage = () => {
   }, []);
   return (
     <>
-
-      {/* <section className="max-w-full md:w-5/6 max-h-full">
-
-        <div className="relative overflow-hidden rounded-lg grid-cols-3 grid-rows-1 shadow-2xl  lg:pb-0 grid gap-1.5 md:grid-cols-3 h-auto sm:grid-cols-1 max-h-full"> */}
-          {/* <div className=" p-6 bg-gray-100 rounded ml-4">
-            <div className="flex justify-center">
-              <form
-                id="form"
-                onSubmit={handleRoute}
-              >
-                <div className="w-80">
-                  <label htmlFor="source" className="block text-sm font-medium text-gray-700">
-                    Source
-                  </label>
-                  <select
-                    type="text"
-                    id="origin"
-                    name="origin"
-                    ref={selectOne}
-                    onChange={handleChange}
-                    placeholder="your current location"
-                    className="w-full rounded-md px-9 py-2 mt-1 text-sm outline-none border-2 border-gray-200 focus:border-indigo-500"
-                  >
-                    <option id="origin-select">Select Origin</option>
-                    {options.map((option) => {
-                      return (
-                        <option
-                          value={option.name}
-                          key={option.name}
-                          className="cursor-pointer bg-transparent font-bold font-raleway disabled:text-gray-400 disabled:bg-gray-100"
-                        >
-                          {option.name}
-                        </option>
-                      );
-                    })}
-                  </select>
-                  <div className="flex justify-center">
-                    <div className="w-80">
-                      <label htmlFor="destination" className="block text-sm font-medium text-gray-700">
-                        Destination
-                      </label>
-                      <select
-                        onChange={handleChangeDes}
-                        type="text"
-                        id="destination"
-                        name="destination"
-                        placeholder="your destination"
-                        className="w-full rounded-md px-4 py-2 mt-1 text-sm outline-none border-2 border-gray-200 focus:border-indigo-500"
-                        ref={selectDes}
-                      >
-                        <option value="" hidden>
-                          Select Destination
-                        </option>
-                        {options.map((option) => {
-                          return (
-                            <option
-                              value={option.name}
-                              key={option.name}
-                              className="cursor-pointer bg-transparent hover:bg-primary font-raleway font-bold disabled:text-gray-400 disabled:bg-gray-100"
-                            >
-                              {option.name}
-                            </option>
-                          );
-                        })}
-                      </select>
-                    </div>
-                  </div>
-                  <div className="flex justify-center space-x-3 m-3">
-                    <Button
-                      name="Track"
-                      styles="bg-primary text-sm text-background py-1"
-                      className="inline-flex items-center px-6 py-2 text-white bg-blue-700 border border-blue-700 rounded hover:bg-transparent hover:text-indigo-600 active:text-indigo-500 focus:outline-none focus:ring"
-                    />
-                  </div>
-                  <div className="flex justify-center gap-4">
-                    <button
-                      onClick={handlestart}
-                      data-tip="Move Bus"
-                      className="bg-green-600 text-white w-10 h-10 rounded-full flex justify-center items-center p-4"
-                    >Start
-                    </button>
-                    <button
-                      onClick={handlestop}
-                      data-tip="Stop Bus"
-                      className="bg-red-600 text-white w-10 h-10 rounded-full flex justify-center items-center p-4"
-                    >Stop
-                    </button>
-                  </div>
-                </div>
-              </form>
-
-
-            </div>
-          </div> */}
       <Logout />
-<div className='absolute flex flex-col'>
+      <div className='absolute flex flex-col'>
 
 
-          <form
-            id="form"
-            onSubmit={handleRoute}
-            className="fixed z-20 flex flex-col justify-center "
-          >
-            <div className="flex flex-col bg-background p-4 rounded-xl mt-2 ml-6 md:ml-10 md:items-baseline">
-              <select
-                type="text"
-                id="origin"
-                name="origin"
-                ref={selectOne}
-                onChange={handleChange}
-                placeholder="your current location"
-                className="rounded-xl bg-background border-primary text-sm outline-none mb-2 pl-4 md:pl-8 py-1  w-40 md:w-56 border-2"
-              >
-                <option id="origin-select">Select Origin</option>
-                {options.map((option) => {
-                  return (
-                    <option
-                      value={option.name}
-                      key={option.name}
-                      className="cursor-pointer bg-transparent font-bold font-raleway disabled:text-gray-400 disabled:bg-gray-100"
-                    >
-                      {option.name}
-                    </option>
-                  );
-                })}
-              </select>
-              <select
-                onChange={handleChangeDes}
-                type="text"
-                id="destination"
-                name="destination"
-                placeholder="your destination"
-                className="rounded-xl bg-background border-primary text-sm outline-none mb-2 pl-4 md:pl-8 py-1 w-40 md:w-56 border-2"
-                ref={selectDes}
-              >
-                <option value="" hidden>
-                  Select Destination
-                </option>
-                {options.map((option) => {
-                  return (
-                    <option
-                      value={option.name}
-                      key={option.name}
-                      className="cursor-pointer bg-transparent hover:bg-primary font-raleway font-bold disabled:text-gray-400 disabled:bg-gray-100"
-                    >
-                      {option.name}
-                    </option>
-                  );
-                })}
-              </select>
-              <div type="submit" className="flex justify-center md:ml-16">
-                <Button
-                  name="Track"
-                  styles="bg-primary text-sm text-background py-1"
-                />
-                
-              </div>
-              <div className="flex justify-center gap-4">
-                    <button
-                      onClick={handlestart}
-                      data-tip="Move Bus"
-                      className="bg-green-600 text-white w-10 h-10 rounded-full flex justify-center items-center p-4"
-                    >Start
-                    </button>
-                    <button
-                      onClick={handlestop}
-                      data-tip="Stop Bus"
-                      className="bg-red-600 text-white w-10 h-10 rounded-full flex justify-center items-center p-4"
-                    >Stop
-                    </button>
-                  </div>
-            </div>
-          </form>
-
-          <div className="ml-2 text-center z-10 mt-[-10px]">
-            <MapContainer
-              center={{ lat: -1.936671, lng: 30.053524 }}
-              zoom={13}
-              zoomControl={false}
-              className="h-[88vh] w-[100vw]"
+        <form
+          id="form"
+          onSubmit={handleRoute}
+          className="fixed z-20 flex flex-col justify-center "
+        >
+          <div className="flex flex-col bg-background p-4 rounded-xl mt-2 ml-6 md:ml-10 md:items-baseline">
+            <select
+              type="text"
+              id="origin"
+              name="origin"
+              ref={selectOne}
+              onChange={handleChange}
+              placeholder="your current location"
+              className="rounded-xl bg-background border-primary text-sm outline-none mb-2 pl-4 md:pl-8 py-1  w-40 md:w-56 border-2"
             >
-              <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              <option id="origin-select">Select Origin</option>
+              {options.map((option) => {
+                return (
+                  <option
+                    value={option.name}
+                    key={option.name}
+                    className="cursor-pointer bg-transparent font-bold font-raleway disabled:text-gray-400 disabled:bg-gray-100"
+                  >
+                    {option.name}
+                  </option>
+                );
+              })}
+            </select>
+            <select
+              onChange={handleChangeDes}
+              type="text"
+              id="destination"
+              name="destination"
+              placeholder="your destination"
+              className="rounded-xl bg-background border-primary text-sm outline-none mb-2 pl-4 md:pl-8 py-1 w-40 md:w-56 border-2"
+              ref={selectDes}
+            >
+              <option value="" hidden>
+                Select Destination
+              </option>
+              {options.map((option) => {
+                return (
+                  <option
+                    value={option.name}
+                    key={option.name}
+                    className="cursor-pointer bg-transparent hover:bg-primary font-raleway font-bold disabled:text-gray-400 disabled:bg-gray-100"
+                  >
+                    {option.name}
+                  </option>
+                );
+              })}
+            </select>
+            <div type="submit" className="flex justify-center md:ml-16">
+              <Button
+                name="Track"
+                styles="bg-primary text-sm text-background py-1"
               />
-              <ZoomControl position="topright" />
-              {origin && destination && currentTrack ? (
-                <BusTracker data={currentTrack} />
-              ) : (
-                ''
-              )}
 
-              {origin && destination ? (
-                <RoutingMachine
-                  ref={rMachine}
-                  origin={origin}
-                  destination={destination}
-                  userPosition={position}
-                />
-              ) : (
-                ''
-              )}
-            </MapContainer>
+            </div>
+            <div className="flex justify-center gap-4">
+              <button
+                onClick={handlestart}
+                data-tip="Move Bus"
+                className="bg-green-600 text-white w-10 h-10 rounded-full flex justify-center items-center p-4"
+              >Start
+              </button>
+              <button
+                onClick={handlestop}
+                data-tip="Stop Bus"
+                className="bg-red-600 text-white w-10 h-10 rounded-full flex justify-center items-center p-4"
+              >Stop
+              </button>
+            </div>
+            <div className="flex justify-center gap-4">
+              <button
+                onClick={addPassenger}
+                className="bg-green-600 text-white w-10 h-10 rounded-full flex justify-center items-center p-4"
+              >addPassenger
+              </button>
+              <button
+                onClick={removePassenger}
+                className="bg-red-600 text-white w-10 h-10 rounded-full flex justify-center items-center p-4"
+              >Remove Passenger
+              </button>
+            </div>
+            
           </div>
-          </div>
-        {/* </div>
-      </section> */}
+          <div>
+              <span>PASSENGERS</span>
+            </div>
+            <div>
+              <span>{passengers}</span>
+            </div>
+        </form>
+
+        <div className="ml-2 text-center z-10 mt-[-10px]">
+          <MapContainer
+            center={{ lat: -1.936671, lng: 30.053524 }}
+            zoom={13}
+            zoomControl={false}
+            className="h-[88vh] w-[100vw]"
+          >
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <ZoomControl position="topright" />
+            {origin && destination && currentTrack ? (
+              <BusTracker data={currentTrack} />
+            ) : (
+              ''
+            )}
+
+            {origin && destination ? (
+              <RoutingMachine
+                ref={rMachine}
+                origin={origin}
+                destination={destination}
+                userPosition={position}
+              />
+            ) : (
+              ''
+            )}
+          </MapContainer>
+        </div>
+      </div>
+
     </>
   );
 };

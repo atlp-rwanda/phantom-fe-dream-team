78b1d279e047cf3efeb30bc2892a613bb234e9f6
GYSSAGARA Prince <andelarwanda@Andelas-MacBook-Pro.local>
2022-07-15 10:42:40 +0200

    Integrate assign drivers to bus

--- src/components/Assign-drivers-to-buses/assign.js
+++ src/components/Assign-drivers-to-buses/assign.js
@@ -1,42 +1,73 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import TopNavbar from '../Dashboard/TopNavbar';
-
+import {backendUrl} from "../../utils/Api.js"
+import { Icon } from '@iconify/react';
 
 
 function AssignDrivers() {
 
     const [data, setData] = useState("");
     const [loading, setLoading] = useState(true)
+    const [Plate, setBusPlate] = useState([]);
+    const loggedin =  localStorage.getItem("auth-token")
+    const [load,setLoad] = useState(false)
     
 
   
-function deleted(ID) {
-    fetch('http://localhost:8000/assignDrivers/' + ID, {
-        method: 'DELETE'
-      }).then(() => {
-      window.location.reload()
-    })
-}
+function Unassign(i,a) {
+    let Bs = Plate.find(o => o.plate === a);
+    setLoad(true)
+    const j=Bs.id
+    fetch(backendUrl+`driverstobuses/unassign/driver/${i}/bus/${j}`, 
+    { method: 'PUT', 
+    headers: { "Content-Type": "application/json",'Authorization': `Bearer ${loggedin}`,}})
+      .then(res => {
+        if (!res.ok) { // get the error from server
+          throw Error('could not fetch the data for that resource');
+        }
+        return res.json();
+      })
+      .then(data => {
+      }).catch(err => {
+        // cathes Network/connection error
+        setLoading(false);
+        setError(err.message);
+      })
+      setTimeout(() => {
+       setLoad(false)
+        window.location.reload()
+      }, 1000);
+    }
+
     
     
 
     useEffect(() => {
-        fetch('http://localhost:8000/assignDrivers')
-            .then(res => {
-                if (!res.ok) { // get the error from server
-                    throw Error('could not fetch the data for that resource');
-                }
-                return res.json();
+        fetch(`${backendUrl}users`, {
+            method: "GET",
+            headers: { "Content-Type": "application/json","Authorization": `Bearer ${loggedin}`},
+        
+        
+        
+          })
+            .then((res) => {
+              return res.json();
             })
-            .then(data => {
-                setData(data);
-                setLoading(false);
-                setError(null);
-            }).catch(err => {
-                // cathes Network/connection error
-                setLoading(false);
-                setError(err.message);
-            })
+            .then((result) => {
+              console.log(result.data)
+              setData(result.data)
+            }),
+            fetch(backendUrl+'buses', 
+{ method: 'GET', 
+headers: { "Content-Type": "application/json",'Authorization': `Bearer ${loggedin}`,}})
+  .then(res => {
+    return res.json();
+  })
+  .then(data => {
+    setBusPlate(data);
+  }).catch(err => {
+  })
+           
     }, []);
 
     return (
@@ -46,7 +77,9 @@ function deleted(ID) {
 
             {/*On Phone */}
                 
-            {data && data.map((bus) => {
+            {data && data.map((Driver) => {
+                if(Driver.plateNumber!=null && Driver.role=="Driver"){
+                    console.log(Driver)
                 return (
                     <div className="p-5 bg-gray-100 sm:block lg:hidden md:hidden 2xl:hidden mt-5 sm:flex-col">
                         <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 sm:block lg:hidden md:hidden 2xl:hidden">
@@ -54,32 +87,37 @@ function deleted(ID) {
                                 <div className="flex items-center space-x-2 text-sm">
                                     <div className='flex flex-row'>
                                         <p className='mr-2'>Drivers: </p>
-                                        <p className="text-blue-500 font-bold hover:underline" >{bus.Driver}</p>
+                                        <p className="text-blue-500 font-bold hover:underline" >{Driver.names}</p>
                                     </div>   
                                 </div>
                                 
                                 <div className="text-sm text-gray-700 flex flex-row">
                                     <p className='mr-2'>BUSES: </p>
-                                    {bus.Plate}
+                                    {Driver.plateNumber}
                                 </div>
                                 <div className="text-sm font-medium text-black flex flex-row">
                                     <p className='mr-2'>ACTION: </p>
-                                    <p className="text-sm font-medium text-black flex flex-row"> <button onClick={
-                                        () => deleted(bus.id)
-                                    }>DELETE</button></p>
+                                    <p className="text-sm font-medium text-black flex flex-row">{!load && <button onClick={
+                                        () => Unassign(Driver.id,Driver.plateNumber)
+                                    }>UNASSIGN</button>}
+                                   {load &&<button onClick={
+                                        () => Unassign(Driver.id,Driver.plateNumber)
+                                    }>
+                                    <Icon icon="eos-icons:three-dots-loading" color="blue" /></button>}
+                                    </p>
                                 </div>
                             </div>
                     </div>
                    </div>
                 )
-            }
+            }}
             )}
 
             {/*On large screen */}
 
             <div className="relative overflow-x-auto  justify-center text-center flex mt-10 mb-40 ">
-                <table className="w-full  text-left text-gray-700 dark:text-gray-400 sm:hidden lg:block md:block 2xl:block">
-                    <thead className="text-xs text-gray-700 uppercase bg-gray-200 dark:bg-gray-700 dark:text-gray-400">
+                <table className="w-full border-black text-left text-gray-700 dark:text-gray-400 sm:hidden lg:block md:block 2xl:block">
+                    <thead className="mb-12 text-xl text-blue-700 bg-gray-200 border-solid border-2 sm:text-sm">
                         <tr>
                             <th scope="col" className="px-16 py-3">DRIVER</th>
                             <th scope="col" className="px-16 py-3">BUS</th>
@@ -87,19 +125,24 @@ function deleted(ID) {
                         </tr>
                     </thead>
                     <tbody>
-                        {data && data.map((bus) => {
+                        {data && data.map((Driver) => {
+                            if(Driver.plateNumber!=null && Driver.role=="Driver"){
                             return (
-                                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                                    <td scope="row" className="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">{bus.Driver}</td>
-                                    <td scope="col" className="px-16 py-3">{bus.Plate}</td>
+                                <tr className="bg-white text-black border-b dark:bg-white dark:border-gray-700">
+                                    <td scope="row" className="px-6 py-4 font-medium text-black dark:text-black whitespace-nowrap">{Driver.names}</td>
+                                    <td scope="col" className="px-16 py-3">{Driver.plateNumber}</td>
                                     <td scope="col" className="px-16 py-3">
-                                    <button onClick={
-                                        () => deleted(bus.id)
-                                    }>DELETE</button>
+                                    {!load && <button onClick={
+                                        () => Unassign(Driver.id,Driver.plateNumber)
+                                    }>UNASSIGN</button>}
+                                   {load &&<button onClick={
+                                        () => Unassign(Driver.id,Driver.plateNumber)
+                                    }>
+                                    <Icon icon="eos-icons:three-dots-loading" color="blue" width="60" height="60" /></button>}
                                     </td>                                  
                                 </tr>
                             )
-                        }
+                        }}
                         )}
                     </tbody>
                 </table>
@@ -108,7 +151,7 @@ function deleted(ID) {
 
         </>
 
-                                              
+                                                            
                                     
 )}
 export default AssignDrivers;
--- src/components/RegisterBuses/registerBus.js
+++ src/components/RegisterBuses/registerBus.js
@@ -9,6 +9,7 @@ function AddBus() {
   const dispatch = useDispatch();
   const [succeed, setSucceed] = useState(false);
   const [error, setError] = useState(false);
+  const [load,setLoad] = useState(false)
   const [Role, setRole] = useState('');
   const [Description, setDescription] = useState('');
   const [seat, setSeat] = useState('');
@@ -18,10 +19,18 @@ function AddBus() {
       document.getElementById("error").innerHTML = 'Bus data must be filled !!!';
     }
     else {
+      setLoad(true)
       const bus = {plate:Role, busType:Description, seat:seat};
      dispatch(addBus(bus))
      setTimeout(() => {
-      setSucceed(true)
+      const error = localStorage.getItem("error");
+      if(error!="null"){
+        setLoad(false)
+        setError(true)
+      }else{
+        setLoad(false)
+        setSucceed(true)
+      }
     }, 1000);
     }
   }
@@ -77,8 +86,18 @@ function AddBus() {
             </button>
           </div>
           <div>
+            {!load &&
             <button onClick={() => submitForm()} className=" lg:mt-5 bg-blue-700 text-white hover:bg-white hover:border-solid hover:border-2 hover:border-blue-600  hover:text-blue-700 font-bold py-1 px-6 rounded xl:text-xs  lg:text-base md:text-xs m:text-xs xs:text-xs">Save</button>
-          </div>
+            }{load &&
+              <button  className='px-4 py-1 rounded-lg bg-blue-100 text-blue-900 border-2 border-blue-600  hover:bg-white hover:border-solid hover:border-2 hover:border-blue-600  hover:text-blue-700 font-bold py-2 px-8 rounded xl:text-xs md:py-1 md:px-12  lg:text-base md:text-xs m:text-xs xs:text-xs xs:py-2'> 
+              <svg role="status" class="inline w-4 h-4 mr-2 text-gray-900 animate-spin dark:text-gray-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+              <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="#1C64F2"/>
+              </svg>
+              Loading...
+          </button>
+              }
+            </div>
         </div>
 
 
